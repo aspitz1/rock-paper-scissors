897ca3313d49fb30704406cd75d4f29b80d76ae5
Anna Spitz <aspitzemail@gmail.com>
2022-06-29 15:07:54 -0500

    Refactore Game Player classes

--- src/player.js
+++ src/player.js
@@ -1,35 +1,11 @@
 class Player {
   constructor(token) {
     this.playerToken = token;
-    this.fighter;
     this.wins = 0;
-    this.human = token === 'human';
-    this.isTurn = false;
+    this.isHuman = token === 'human';
   }
 
-  addWin(){
+  addWin() {
     this.wins++;
   }
-//object
-  chooseFighter(eventTargetDataAttribute, currentGameStyle) {
-    for (let i = 0; i < currentGameStyle.length; i++) {
-      if (eventTargetDataAttribute === currentGameStyle[i]) {
-        this.fighter = currentGameStyle[i];
-      }
-    }
-  }
-
-  randomFighter(currentGameStyle) {
-      const num = Math.floor(Math.random() * currentGameStyle.length);
-      this.fighter = currentGameStyle[num];
-  }
-
-  changeTurns() {
-    if(this.isTurn) {
-      this.isTurn = false;
-    } else {
-      this.isTurn = true;
-    }
-  }
-
 }
